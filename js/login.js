b72e84e246245f89fb49d596b7f05f7e2ac81fb7
Robert Yanez Evariste <ryaneze@sonda.com.uy>
2022-08-12 12:13:28 -0300

    Se modifica JS para mensajes por span

--- js/login.js
+++ js/login.js
@@ -1,13 +1,30 @@
 function login(){
     let correo = document.getElementById("email").value;
     let contra = document.getElementById("contra").value;
+    let emafail = document.getElementById('emafail');
+    let passfail = document.getElementById('passfail');
 
-    if (correo!="" && contra!=""){
-        location.href="index.html"
-    }
-    else{
-        alert("Usuario y clave son requeridos");
-    }
+        if (correo=="" && contra=="") {
+            emafail.innerHTML = "Debe ingresar su e-mail";
+            passfail.innerHTML = "Debe ingresar su password";
+        }
+        else { 
+            if (correo=="" && contra!="") {
+                emafail.innerHTML = "Debe ingresar su e-mail";
+                passfail.innerHTML = "";
+            }
+            else {
+                if (correo!="" && contra=="") {
+                    passfail.innerHTML = "Debe ingresar su password";
+                    emafail.innerHTML = "";
+                }
+                else {
+                    if (correo!="" && contra!=""){
+                    location.href="index.html"
+                    }
+                }
+            }
+        }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
